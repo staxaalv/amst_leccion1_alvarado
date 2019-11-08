38e009b64f4aa20e323838ff5489858ed7b1aaff
kavelino <kleberth@icloud.com>
2019-11-08 12:19:45 -0500

    creacion de proyecto

---
+++ app/src/main/java/com/example/amst_l1_alvarado_espolservicios/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.amst_l1_alvarado_espolservicios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
